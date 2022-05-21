290b850c24d8c881374fb78d386f7e8692cf3d1a
sglbl <silo1881@gmail.com>
2022-05-21 14:32:08 +0300

    n m

--- main.c
+++ main.c
@@ -49,10 +49,6 @@ int main(int argc, char *argv[]){
     if( statOfFile[0].st_size < pow(2,n)*pow(2,n) || statOfFile[1].st_size < pow(2,n)*pow(2,n) ){
         write(STDERR_FILENO,"Error. Size of file is less than (2^n)*(2^n)\n", 45);  exit(EXIT_FAILURE);
     }
-    // Checking divisibility
-    if( m > pow(2,n) ){
-        write(STDERR_FILENO,"Error. m should be less than or equal to 2^n\n", 45);  exit(EXIT_FAILURE);
-    }
 
     signalHandlerInitializer();
     int fileDescs[3];
