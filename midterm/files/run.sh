a9abb1999d30604f848bd515263fd3d36b4d73d8
Süleyman Gölbol <64928475+sglbl@users.noreply.github.com>
2025-05-11 14:07:58 +0200

    Update run shell script

--- midterm/files/run.sh
+++ midterm/files/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./client -s ../../../serverFifo -o matrix1.csv &
-./client -s ../../../serverFifo -o matrix2.csv &
-./client -s ../../../serverFifo -o matrix3.csv &
-./client -s ../../../serverFifo -o matrix1.csv &
-./client -s ../../../serverFifo -o matrix2.csv &
-./client -s ../../../serverFifo -o matrix3.csv
+./client -s serverFifo -o files/matrix1.csv &
+./client -s serverFifo -o files/matrix2.csv &
+./client -s serverFifo -o files/matrix3.csv &
+./client -s serverFifo -o files/matrix1.csv &
+./client -s serverFifo -o files/matrix2.csv &
+./client -s serverFifo -o files/matrix3.csv
 
 
